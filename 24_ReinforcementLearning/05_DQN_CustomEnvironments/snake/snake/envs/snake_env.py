8873a01c0bf3de9e7560fce1ab76ba2be4f9c673
Mikel Sagardia <mxagar@gmail.com>
2022-02-08 10:10:54 +0100

    snake env definition

--- 24_ReinforcementLearning/05_DQN_CustomEnvironments/snake/snake/envs/snake_env.py
+++ 24_ReinforcementLearning/05_DQN_CustomEnvironments/snake/snake/envs/snake_env.py
@@ -34,7 +34,7 @@ class SnakeEnv(gym.Env):
         #self.frame_size_y = frame_size_y
         self.frame_size_x = 200
         self.frame_size_y = 200
-        self.action_space = spaces.Discrete(4)
+        self.action_space = spaces.Discrete(4) # [0, 1, 2, 3] = [UP, DOWN, LEFT, RIGHT]
         # We create a pygame window with the window size
         self.game_window = pygame.display.set_mode((self.frame_size_x, self.frame_size_y))
         # How long Gym trains, to avoid looping forever - it depends on the game
@@ -47,6 +47,7 @@ class SnakeEnv(gym.Env):
     def reset(self):
         '''
         Resets the game, along with the default snake size and spawning food.
+        It ONLY outputs the observation = img, in our case
         '''
         # Reset initializes the state
         self.game_window.fill(BLACK)
@@ -66,26 +67,36 @@ class SnakeEnv(gym.Env):
         self.action = self.direction
         self.score = 0
         self.steps = 0
-        print("Game Reset.")
-        
-    def change_direction(self, action, direction):
+        #print("Game Reset.")
+        # Grab, re-define axes and return image!
+        img = array3d(display.get_surface())
+        img = np.swapaxes(img,0,1)
+        return img
+    
+    # This is a static method: the class is not changed by it.
+    # Thus, we remove self from args and add @staticmethod decorator
+    @staticmethod    
+    def change_direction(action, direction):
         '''
         Changes direction based on action input. 
         Checkes to make sure snake can't go back on itself.
         '''
         # Check that we don't go back against the snake
-        if action == 'UP' and direction != 'DOWN':
+        # [0, 1, 2, 3] = [UP, DOWN, LEFT, RIGHT]
+        # We need to modify action, but we can leave direction
+        if action == 0 and direction != 'DOWN':
             direction = 'UP'
-        if action == 'DOWN' and direction != 'UP':
+        if action == 1 and direction != 'UP':
             direction = 'DOWN'
-        if action == 'LEFT' and direction != 'RIGHT':
+        if action == 2 and direction != 'RIGHT':
             direction = 'LEFT'
-        if action == 'RIGHT' and direction != 'LEFT':
+        if action == 3 and direction != 'LEFT':
             direction = 'RIGHT'
         # Current direction is changed iff we don't go back against the snake
         return direction
     
-    def move(self, direction, snake_pos):
+    @staticmethod
+    def move(direction, snake_pos):
         '''
         Updates snake_pos list to reflect direction change.
         '''
@@ -118,6 +129,7 @@ class SnakeEnv(gym.Env):
         # is in the same position as the food
         return self.snake_pos[0] == self.food_pos[0] and self.snake_pos[1] == self.food_pos[1]
     
+    """
     def human_step(self, event):
         '''
         Takes human keyboard event and then returns it as an action string.
@@ -147,7 +159,38 @@ class SnakeEnv(gym.Env):
             if event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event(pygame.QUIT))
         return action
+    """
     
+    # human_step() is removed and we create instead step, with new code
+    # step() is a standard method required by OpenAI Gym
+    def step(self, action):
+        '''
+        What happens when the agent performs the action on the env?
+        '''
+        scoreholder = self.score
+        reward = 0
+        # Execute the action
+        self.direction = SnakeEnv.change_direction(action, self.direction)
+        # Apply the effect of the action - we have it in two steps
+        self.snake_pos = SnakeEnv.move(self.direction, self.snake_pos)
+        # Insert new square to the front
+        # If food not eaten, back square is removed by food_handler()
+        # We simulate movement like that
+        self.snake_body.insert(0, list(self.snake_pos))
+        # In a general case it would be reward_handler(), we report back the reward
+        reward = self.food_handler() 
+        # Update game/env state
+        self.update_game_state()
+        # Pack results
+        reward, done = self.game_over(reward)
+        img = self.get_image_array_from_game()
+        info = {"score": self.score}
+        self.steps += 1
+        time.sleep(self.sleep)
+        # observation = img; we will train CNNs with images
+        return img, reward, done, info
+
+    """
     def display_score(self, color, font, size):
         '''
         Updates the score in top left.
@@ -163,7 +206,62 @@ class SnakeEnv(gym.Env):
         score_rect.midtop = (self.frame_size_x/10, 15)
         # Put it on the snake window
         self.game_window.blit(score_surface, score_rect)
-    
+    """
+    def food_handler(self):
+        if self.eat():
+            self.score += 1
+            reward = 1
+            self.food_spawn = False
+        else:
+            # Remove last square: simulate movement
+            # Recall we insert a square to the front in step()
+            self.snake_body.pop()
+            reward = 0
+
+        if not self.food_spawn:
+            self.food_pos = self.spawn_food()
+        self.food_spawn = True
+
+        return reward
+
+    def update_game_state(self):
+        self.game_window.fill(BLACK)
+        for pos in self.snake_body:
+            pygame.draw.rect(self.game_window, GREEN, pygame.Rect(pos[0], pos[1], 10, 10))
+
+        pygame.draw.rect(self.game_window, WHITE, pygame.Rect(self.food_pos[0], self.food_pos[1], 10, 10))
+
+    def get_image_array_from_game(self):
+        img = array3d(display.get_surface())
+        img = np.swapaxes(img, 0, 1)
+        return img
+
+    def game_over(self, reward):
+        # We need to punish if wall is hit or if we go against the snake
+        # Touch box
+        if self.snake_pos[0] < 0 or self.snake_pos[0] > self.frame_size_x-10:
+            return -1, True
+        if self.snake_pos[1] < 0 or self.snake_pos[1] > self.frame_size_y-10:
+            return -1, True
+        # Touch own body
+        for block in self.snake_body[1:]:
+            if self.snake_pos[0] == block[0] and self.snake_pos[1] == block[1]:
+                return -1, True
+        # We decide not to punish the agent
+        if self.steps >= self.STEP_LIMIT:
+            return 0, True
+        
+        # reward, done
+        return reward, False
+
+    def render(self, mode='human'):
+        if mode == "human":
+            display.update()
+            
+    def close(self):
+        pass
+
+    """
     def game_over(self):
         '''
         Checks if the snake has touched the bounding box or itself.
@@ -178,7 +276,9 @@ class SnakeEnv(gym.Env):
         for block in self.snake_body[1:]:
             if self.snake_pos[0] == block[0] and self.snake_pos[1] == block[1]:
                 self.end_game()
+    """
 
+    """
     def end_game(self):
         '''
         Clean up and end the game.
@@ -196,3 +296,4 @@ class SnakeEnv(gym.Env):
         time.sleep(3)
         pygame.quit()
         sys.exit()
+    """
